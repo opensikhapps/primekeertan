a130aaf1aa982e0060bcb21dc51ecb4d9d10b181
cingh-jasdeep <cingh.jasdeep@gmail.com>
2013-08-06 23:01:18 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 var $ky_list;
-$( document ).on( "pageshow", "#home_loading_page", function() {
-            showLoading("Fetching Keertaneeye list...");
+$( document ).one( "pageshow", "#home_loading_page", function() {
             $ky_list = $( '#ky_search' );
-            //fetch list from yql
+            fetch_keertaneeye(true);
+});
+
+$( document ).one( "pageshow", "#ky_list_page", function() {
+            $ky_list = $( '#ky_search' );
+            if($ky_list.is(':empty')){
+                fetch_keertaneeye(false);
+            }
 });
 
 function showLoading(load_text){
@@ -20,11 +26,49 @@ function hideLoading(){
     }, 1); 
 }
 
-//funtion fetch_keertaneeye() {
-    
-//} 
+function fetch_keertaneeye(isHome) {
+    showLoading("Fetching Keertaneeye list...");
+    var html = "";
+    var statement = 'select content from html \n\
+                    where url="http://akj.org/skins/one/search.php" and \n\
+                    xpath=\'//select[contains(@name,"keertaneeya")]/option\' ';
+    $.queryYQL(statement, function (data) {
+        var data_ky_list = data.query.results.option;
+        //console.log(data_ky_list);
+        $.each(data_ky_list, function (i,val) {
+            if(val) {
+                html += "<li class='ky'><a href='#'>" + val + "</a></li>";
+            }
+        });
+        $ky_list.html( html );
+        hideLoading();
+        if(isHome) {
+            goto_ky_list_page();
+        } else {
+            $ky_list.listview( "refresh" );
+            $ky_list.trigger( "updatelayout");
+        }
+        $ky_list.children('li.ky').click(function(){
+            var ky = $(this).find('a').text();
+            fetch_mp3_links(ky);
+        });
+    });
+} 
 
 function goto_ky_list_page() {
     hideLoading();
-    $.mobile.changePage("#ky_list_page", {transition : "slide", changeHash: false });
+    $.mobile.changePage("#ky_list_page", {transition : "slide"});
+}
+
+function fetch_mp3_links(ky) {
+    showLoading("Loading MP3 list...");
+    var statement = 'select * from htmlpost where url="http://akj.org/skins/one/results.php" and \n\
+                    postdata="keertaneeya='+ky+'" and \n\
+                    xpath="//img[contains(@alt,\'MP3\')]/../../..//a"';
+    console.log(statement);
+    $.queryYQL(statement, "all", function (data) {
+        var data_mp3_list = data.query.results.postresult.a;
+        console.log(data_mp3_list);
+        hideLoading();
+    });
 }
